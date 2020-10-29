bfdaf5af7f5cfa5bea471ec2f6376f67d831819f
Shreya0904 <70674052+Shreya0904@users.noreply.github.com>
2020-10-29 16:17:52 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,20 +11,24 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1200, 700);
 
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 	engine = Engine.create();
 	world = engine.world;
 
+	var options= {
+		isStatic:false,
+		restitution:3,
+		friction:0.5,
+		densiny:1.2
+		
+		}
+
 	//Create the Bodies Here.
-    var options= {
-	isStatic:false,
-	restitution:3,
-	friction:0.5,
-	densiny:1.2
-	
-	}
+    paper = new Ground (30,645,20,20);
   
 
 	function keyPressed(){
@@ -35,6 +39,20 @@ function setup() {
 	 }	
 	}
 
+	boxCenterSprite= createSprite(965,667,150,20);
+	boxCenterSprite.shapeColor="red";	
+	
+	boxRightSprite= createSprite(1060,610,30,100);
+	boxRightSprite.shapeColor="red";	
+	
+	boxLeftSprite= createSprite(870,615,30,100);
+	boxLeftSprite.shapeColor="red";	
+	
+		 ground = new Ground(400,665,800,30);
+		 boxCenter = new Ground(965,667,150,20);
+		 boxRight = new Ground(1060,610,200,20);
+		 boxLeft = new Ground(870,615,30,100);
+
 	Engine.run(engine);
   
 }
@@ -44,6 +62,9 @@ function draw() {
   rectMode(CENTER);
   background(0);
   
+  text(mouseX+","+mouseY,mouseX,mouseY);
+
+  paper.display();
   drawSprites();
  
 }
